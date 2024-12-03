27144395d1a44ed62646c796f46f65b94be8c6d7
Giordon Stark <kratsg@gmail.com>
2024-12-02 20:12:21 -0800

    fix up project/subproject and add component code for yy

--- src/itksn/core.py
+++ src/itksn/core.py
@@ -21,24 +21,43 @@ SerialNumberStruct = "SerialNumber" / Struct(
     ),
     "project_code"
     / EnumStr(
-        Bytes(2),
-        inner_pixel=b"PI",
-        outer_pixel_barrel=b"PB",
-        pixel_general=b"PG",
-        pixel_endcaps=b"PE",
-        strip_general=b"SG",
-        strip_barrel=b"SB",
-        strip_endcaps=b"SE",
-        common_mechanics=b"CM",
-        common_electronics=b"CE",
+        Bytes(1),
+        pixel=b"P",
+        strip=b"S",
+        common=b"C",
     ),
     "subproject_code"
     / Switch(
-        this.project_code,
+        lambda ctx: ctx.project_code,
+        {
+            "pixel": EnumStr(
+                Bytes(1),
+                inner_pixel=b"I",
+                outer_pixel_barrel=b"B",
+                pixel_general=b"G",
+                pixel_endcaps=b"E",
+            ),
+            "strip": EnumStr(
+                Bytes(1),
+                strip_general=b"G",
+                strip_barrel=b"B",
+                strip_endcaps=b"E",
+            ),
+            "common": EnumStr(
+                Bytes(1),
+                common_mechanics=b"CM",
+                common_electronics=b"CE",
+            ),
+        },
+    ),
+    "component_code"
+    / Switch(
+        this.subproject_code,
         {
             "inner_pixel": pixels.subproject_codes["PI"],
             "outer_pixel_barrel": pixels.subproject_codes["PB"],
             "pixel_general": pixels.subproject_codes["PG"],
+            "pixel_endcaps": pixels.subproject_codes["PE"],
         },
         default=PaddedString(2, "utf8"),
     ),
@@ -49,6 +68,7 @@ SerialNumberStruct = "SerialNumber" / Struct(
             "inner_pixel": pixels.identifiers,
             "outer_pixel_barrel": pixels.identifiers,
             "pixel_general": pixels.identifiers,
+            "pixel_endcaps": pixels.identifiers,
         },
         default=Bytes(7),
     ),
--- tests/test_core.py
+++ tests/test_core.py
@@ -20,8 +20,9 @@ def test_parse_fe_wafer():
     parsed = itksn.parse(b"20UPGFW2123456")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "FE_chip_wafer"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "FE_chip_wafer"
     assert parsed.identifier.batch == "RD53A"
     assert parsed.identifier.number == b"2123456"
 
@@ -35,8 +36,9 @@ def test_parse_sensor():
     parsed = itksn.parse(b"20UPGW34212345")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Outer_pixel_sensor_wafer_150um_thickness"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Outer_pixel_sensor_wafer_150um_thickness"
     assert parsed.identifier.manufacturer == "V5_LFoundry"
     assert parsed.identifier.sensor_type == "Halfmoon_preproduction_Double_MS"
     assert parsed.identifier.number == b"12345"
@@ -46,8 +48,9 @@ def test_parse_bare_module():
     parsed = itksn.parse(b"20UPGBS1112345")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Digital_single_bare_module"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Digital_single_bare_module"
     assert parsed.identifier.FE_chip_version == "ITkpix_v1"
     assert parsed.identifier.sensor_type == "Market_survey_sensor_tile"
     assert parsed.identifier.number == b"12345"
@@ -57,8 +60,9 @@ def test_parse_pcb():
     parsed = itksn.parse(b"20UPGPD0112345")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Dual_PCB"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Dual_PCB"
     assert parsed.identifier.FE_chip_version == "RD53A"
     assert parsed.identifier.PCB_manufacturer == "EPEC"
     assert parsed.identifier.number == b"12345"
@@ -68,8 +72,9 @@ def test_parse_module():
     parsed = itksn.parse(b"20UPGR90112345")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Digital_quad_module"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Digital_quad_module"
     assert parsed.identifier.FE_chip_version == "RD53A"
     assert parsed.identifier.PCB_manufacturer == "EPEC"
     assert parsed.identifier.number == b"12345"
@@ -79,8 +84,9 @@ def test_parse_quad_module():
     parsed = itksn.parse(b"20UPIM12602173")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "inner_pixel"
-    assert parsed.subproject_code == "L1_quad_module"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "inner_pixel"
+    assert parsed.component_code == "L1_quad_module"
     assert parsed.identifier.FE_chip_version == "ITkpix_v1p1"
     assert parsed.identifier.PCB_manufacturer == "Yamashita_Material"
     assert parsed.identifier.number == b"02173"
@@ -90,8 +96,9 @@ def test_parse_module_carrier():
     parsed = itksn.parse(b"20UPGMC2291234")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Module_carrier"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Module_carrier"
     assert parsed.identifier.module_type == "Linear_triplet_module_carrier"
     assert parsed.identifier.module_version == "Quad_v2p1"
     assert parsed.identifier.manufacturer == b"9"
@@ -107,15 +114,16 @@ def test_parse_digital_quad_module():
     parsed = itksn.parse(b"20UPGR92101041")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "Digital_quad_module"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "Digital_quad_module"
     assert parsed.identifier.FE_chip_version == "ITkpix_v1p1"
     assert parsed.identifier.PCB_manufacturer == "EPEC"
     assert parsed.identifier.number == b"01041"
 
 
 @pytest.mark.parametrize(
-    ("serial_number", "subproject_code", "assembly_site", "number"),
+    ("serial_number", "component_code", "assembly_site", "number"),
     [
         ("20UPIMS2002140", "Triplet_L0_stave_module", "Genova", b"2140"),
         ("20UPIM52002140", "Triplet_L0_Ring0p5_module", "Genova", b"2140"),
@@ -124,12 +132,13 @@ def test_parse_digital_quad_module():
     ],
     ids=["normal", "R05", "R0", "L0"],
 )
-def test_triplet_modules(serial_number, subproject_code, assembly_site, number):
+def test_triplet_modules(serial_number, component_code, assembly_site, number):
     parsed = itksn.parse(serial_number.encode())
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "inner_pixel"
-    assert parsed.subproject_code == subproject_code
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "inner_pixel"
+    assert parsed.component_code == component_code
     assert parsed.identifier.assembly_site == assembly_site
     assert parsed.identifier.not_used == b"0"
     assert parsed.identifier.number == number
@@ -139,8 +148,9 @@ def test_fe_chip():
     parsed = itksn.parse(b"20UPGFC1048575")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "pixel_general"
-    assert parsed.subproject_code == "FE_chip"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "pixel_general"
+    assert parsed.component_code == "FE_chip"
     assert parsed.identifier.batch == "ITkpix_v1"
     assert parsed.identifier.wafer == 255
     assert parsed.identifier.row == 15
@@ -151,8 +161,9 @@ def test_is_capillary():
     parsed = itksn.parse(b"20UPICP1299999")
     assert parsed.atlas_project == "atlas_detector"
     assert parsed.system_code == "phaseII_upgrade"
-    assert parsed.project_code == "inner_pixel"
-    assert parsed.subproject_code == "IS_capillary"
+    assert parsed.project_code == "pixel"
+    assert parsed.subproject_code == "inner_pixel"
+    assert parsed.component_code == "IS_capillary"
     assert parsed.identifier.production_type == "Production"
     assert parsed.identifier.type == "R01"
     assert parsed.identifier.number == b"99999"
